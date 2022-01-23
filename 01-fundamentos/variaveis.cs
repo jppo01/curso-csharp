6e070184dd3cdfdc756d0a65838a04688ae9235b
João Paulo <jppo@live.com>
2022-01-23 14:26:28 -0300

    tipos de tipagem
    adicionado tipos de tipagem

--- 01-fundamentos/variaveis.cs
+++ 01-fundamentos/variaveis.cs
@@ -34,24 +34,53 @@
 */
 
 
-//=============== Linguagem fortemente tipada ===============\\
 
+//=============== Linguagem fortemente tipada ===============\\
 /*
-	O C# é uma linguagem fortemente tipiada. Isso significa que uma vez que uma variável é criada
+	O C# é uma linguagem fortemente tipiada. Isso significa que uma vez que uma variável é criada e iniciada,
 	o seu tipo não pode ser mudado. 
+    Temos duas formas de criar e iniciar váriáeis, a tipagem estatica e a dinamica. 
 */
 
+//=============== Tipagem Estática ===============\\
+/*
+	A tipagem estática se dá quando na declaração da variável ja é passado o seu tipo.
+    Por exemplo:
+    int numero;
+    ou
+    char letra;
 
+    poderiamos ainda declar e iniciar a variável recebendo um valor
+    nesse caso:
+    int numero = 0; // variável do tipo inteiro recebe valor 0
+    ou 
+    char letra = 'A'; // variável do tipo character recebe A
+
+*/
 
 //=============== Tipagem dinamica ===============\\
 /* 
     É possível declarar uma variável sem informar diretamente o seu tipo.
-    Para isso usamos var. O que irá definir o tipo da variável é o dado que ela recebe.
+    Para isso usamos var. O que irá definir o tipo da variável é o tipo de valor que ela recebe.
     Lembrando que C# possui uma tipagem forte, portanto, uma vez passado o valor da variável
-    ela assume a tipagem e não pode ser inserido novo dado com tipagem diferente.
+    ela assume a tipagem e não pode ser inserido novo valor com tipagem diferente.
     Exemplo: 
+    var numero; // variável sem tipo (por enquanto)
+
+    adicioando um valor a variável 
+    numero = 3; // a váriavel agora só recebe valor inteiro
+
+    o mesmo poderia ser feito com qualquer tipo de dado
     
 */
 
+
 //=============== Conversões de cast e tipo ===============\\
+/*
+    E se quissemos fazer com que uma variável do tipo string recebesse algum valor do tipo int. Como fariamos? 
+    Para isso temos as conversões. 
+
+
+
+*/
 
