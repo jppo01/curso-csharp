e0e6813a3dd0fe98142f5269eab81a151f226de7
João Paulo <jppo@live.com>
2022-01-28 11:35:00 -0300

    Mudança de nome de pasta

---
+++ exercicios/poo/comparar-idade/Program.cs
@@ -0,0 +1,32 @@
+﻿/* exercício: 
+ * Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
+ * velha
+ */
+
+Pessoa x, y;
+x = new Pessoa();
+y = new Pessoa();
+
+//entrada de dados da primeira pessoa
+Console.WriteLine("Dados da primeira pessoa:");
+Console.WriteLine("Nome:");
+x.Nome = Console.ReadLine();
+Console.WriteLine("Idade:");
+x.Idade = int.Parse(Console.ReadLine());
+
+//entrada de dados da segunda pessoa
+Console.WriteLine("Dados da segunda pessoa:");
+Console.WriteLine("Nome:");
+y.Nome = Console.ReadLine();
+Console.WriteLine("Idade:");
+y.Idade = int.Parse(Console.ReadLine()); 
+
+
+// lógica para comparar as idades e printar o resultado
+if (x.Idade > y.Idade)
+{
+    Console.WriteLine(x.Nome + " é o mais velho(a)");
+} else
+{
+    Console.WriteLine(y.Nome + " é o mais velho (a)");
+}
---
+++ exercicios/poo/triangulos/triangulos.cs
@@ -0,0 +1,15 @@
+        class Triangulos {
+            public double A=0;
+            public double B=0;
+            public double C=0; 
+
+            //metodos
+            public double Area()
+            {
+                // formula para calcular area de triangulo
+                double p = (A + B + C)/2.0;
+                double a = Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+
+                return a;
+            }
+    }
