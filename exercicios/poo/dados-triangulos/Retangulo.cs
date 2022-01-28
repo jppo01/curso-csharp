37b5a09e3cb2835a6a0cb16a31ce7164f1d85565
jppo@live.com <jppo@live.com>
2022-01-28 16:09:07 -0300

    adicionado resolução de exercicio: dado largura e altura de um triangulo calcular area, perimetro e diagonal

---
+++ exercicios/poo/dados-triangulos/Retangulo.cs
@@ -0,0 +1,30 @@
+
+public class Retangulo 
+{
+    // Atributos
+    private double Largura;
+    private double Altura;
+
+    // ======== MÉTODOS ========
+
+    // Construtores
+    public Retangulo (double largura, double altura){
+        Largura = largura;
+        Altura = altura;
+    }
+
+    //formulas matemáticas
+    public double Area()
+    {
+        return ((Altura * Largura)/2);
+    }
+    public double Perimetro()
+    {
+        return (2*(Altura + Largura));
+    }
+
+    public double Diagonal()
+    {
+        return (Math.Sqrt(Altura * Altura + Largura * Largura));
+    }
+}
