b2c7cf0ea6b10a895f46e318099eefdaafa5913a
João Paulo <jppo@live.com>
2022-01-29 00:54:33 -0300

    Resolução de Exercício Aprovação de Aluno

---
+++ exercicios-resolvidos/poo/aprovacao-aluno/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+public class Aluno
+{
+	//========= ATRIBUTOS =========
+	private string Nome;
+	private double N1, N2, N3, N4;
+
+
+
+
+	//========= MÉTODOS ===========
+
+	//Construtor
+	public Aluno(string nome, double n1, double n2, double n3)
+	{
+		Nome = nome;
+		N1 = n1;
+		N2 = n2;
+		N3 = n3;
+	}
+
+	//Regras de negócio
+	public void Aprovacao() {
+		double media = Media();
+		 if (media >= 60)
+		{
+			Console.WriteLine("Nota final: " + media.ToString("F2", CultureInfo.InvariantCulture));
+			Console.WriteLine("APROVADO!");
+		} else {
+			Console.WriteLine("Nota final: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("REPROVADO!");
+            Console.WriteLine("FALTARAM: " + NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS!");
+		}
+	}
+
+	//Cálculos matemáticos
+
+	public double Media()
+    {
+		return (N1 + N2 + N3);
+    }
+	
+	public double NotaRestante()
+    {
+		double media = Media();
+		return (60 - media);
+    }
+}
---
+++ exercicios-resolvidos/poo/aprovacao-aluno/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+* Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+* (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+* ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+* para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver
+* este problema.
+* OBS: sem tratamento de exceção! 
+*/
+
+using System.Globalization;
+
+//Declarando Variáveis
+string nome;
+double n1, n2, n3;
+
+//Entrada de dados
+Console.WriteLine("Informe os dados do Aluno");
+Console.Write("Nome: ");
+nome = Console.ReadLine();
+Console.Write("Nota N1: ");
+n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Nota N2: ");
+n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Nota N3: ");
+n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//criando e estanciando objeto
+Aluno A;
+A = new Aluno(nome, n1, n2, n3);
+
+//exibindo resultado
+A.Aprovacao();
