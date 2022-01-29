b53a19706db820fa42ed66029cc1d998252815db
João Paulo <jppo@live.com>
2022-01-29 11:16:58 -0300

    Adicionado Resolução de exercício Conversão USDBRL com valor de IFO

---
+++ exercicios-resolvidos/poo/conversor-moeda/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class ConversorDeMoeda
+{	
+
+	// ============ MÉTODOS =============
+
+	// Comprar Dolar
+	public static double USDBRL(double precoDolar, double qtdDolar)
+	{
+		double IFO = 0.06;
+		return (precoDolar * qtdDolar) + IFO * (precoDolar * qtdDolar);
+
+
+	}
+}
---
+++ exercicios-resolvidos/poo/conversor-moeda/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ *  Exercício usando membros estaticos
+ * OBS: sem tratamento de exceção
+ */
+
+using System.Globalization;
+
+//Declaração de variável
+double cotacao, qtde, apagar;
+
+Console.WriteLine("Qual é a Cotação do dólar? ");
+cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Quantos Dolares você vai comprar? ");
+qtde = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+apagar = ConversorDeMoeda.USDBRL(cotacao, qtde);
+Console.WriteLine("Valor a ser pago em Reais: " + apagar.ToString("F2"));
