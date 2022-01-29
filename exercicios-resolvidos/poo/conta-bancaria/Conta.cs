5203fb3e29bf6612788f8bc50d3f76a5e2684540
João Paulo <jppo@live.com>
2022-01-29 17:42:11 -0300

    Adicionado resolução de exercício canta bancaria

---
+++ exercicios-resolvidos/poo/conta-bancaria/Conta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+public class Conta
+{
+	// ========= ATRIBUTOS ========
+	private string _nome;
+	private double _saldo;
+
+
+	//======= AUTO PROPERTIES =======
+	public int NumConta { get; private set; }
+
+
+	//======== CONSTRUTORES ========
+	public Conta (int conta, string nome) // metodo para ser utilizado sem saldo inicial
+    {
+		NumConta = conta;
+		_nome = nome;
+
+		//não foi realizado atribuição de _saldo = 0 porque por padrão em C# já é atribuido o valor 0;
+    }
+	public Conta (int conta, string nome, double depositoInicial) : this (conta, nome ) // método utiliozado para saldo inicial 
+	{
+		Deposito(depositoInicial);
+	}
+
+
+	//====== PROPERTIES ======
+	public string Nome
+	{
+		get { return _nome; } // retorna o valor de _nome
+		set { _nome = value; } // altera o nome em _nome para o novo parametro passado 
+	}
+
+	public double Saldo
+    {
+		get { return _saldo; } // retorna o valor em _saldo
+		set { _saldo = value;} // adiciona o valor passado como parametro ao valor que já em em _saldo
+    }
+
+
+	//====== OUTROS MÉTODOS DA CLASSE =======
+	public void Deposito(double deposito)
+    {
+		Saldo += deposito; 
+    }
+
+	public void Saque (double saque)
+    {
+		Saldo = Saldo - saque - 5;
+    }
+
+    public override string ToString()
+    {
+        return ("Conta: " + NumConta +", Titular: " + Nome + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
+
---
+++ exercicios-resolvidos/poo/conta-bancaria/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+// Declaração de variável
+int conta;
+string nome;
+char saldoInicial;
+double saldo;
+
+// declarando objeto 
+Conta c;
+
+//entrada
+Console.Write("Numero da conta: ");
+conta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Nome do titular: ");
+nome = Console.ReadLine();
+Console.Write("Haverá depósito inicial (s/n)?");
+saldoInicial = char.Parse(Console.ReadLine());
+
+// lógica para deposito inical e estancimamento de objeto
+if (saldoInicial == 's')
+{
+    Console.Write("Entre o valor de depósito inicial: ");
+    saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    //Estanciando objeto
+    c = new Conta(conta, nome, saldo);
+} else
+{
+    //estanciando objeto
+    c = new Conta(conta, nome);
+}
+
+Console.WriteLine(c);
+
+Console.WriteLine("-----------------------------------------------------------");
+
+// Deposito
+Console.Write("Entre um valor para depósito: ");
+double deposito = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+c.Deposito(deposito);
+Console.WriteLine("Dados atualizado: ");
+Console.WriteLine(c);
+
+Console.WriteLine("------------------------------------------------------------");
+
+//Saque
+Console.Write("Entre um valor para saque: ");
+double saque = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+c.Saque(saque);
+Console.WriteLine("Dados atulizado: ");
+Console.WriteLine(c);
+
+
+
+
