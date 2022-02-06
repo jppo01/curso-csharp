cd80a3d9d35a64622359c388b3351c091ffb09f3
jppo@live.com <jppo@live.com>
2022-02-06 10:37:02 -0300

    Adicionado exercício usando array e poo para calcular média entre n produtos

---
+++ exercicios-resolvidos/arrays-listas/preco-medio-produtos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace preco_medio_produtos {
+        class program{ 
+            static void Main ( string[] args){
+            
+            // declarando variável numero de produtos a serem add
+            int n;
+            double soma = 0;
+            System.Console.WriteLine("Entre c/ quantos produtos serão adicionados");
+            n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Criando vetor do tipo Product
+            Product[] vet = new Product[n];
+
+            for (int i = 0; i<n; i++)
+            {
+                vet[i] = new Product();
+               System.Console.Write("Informe Nome do Produto:");
+               vet[i].Name = Console.ReadLine();
+               System.Console.Write("Informe o preço do produto:");
+               vet[i].Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+  
+               soma += vet[i].Price;
+            }
+
+        double med = soma / n;
+
+        System.Console.WriteLine("Media: " + med.ToString("F2", CultureInfo.InvariantCulture));
+          
+
+        }
+    }
+}
+
+
+
---
+++ exercicios-resolvidos/arrays-listas/preco-medio-produtos/product.cs
@@ -0,0 +1,11 @@
+namespace preco_medio_produtos
+{
+    public class Product
+    {
+        // Auto Properties
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
