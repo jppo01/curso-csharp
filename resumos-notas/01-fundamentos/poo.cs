d8662afd8907ff21a938189e3d05b936abde7758
João Paulo <jppo@live.com>
2022-06-28 15:30:37 -0300

    adicionando arquivo resumo de poo

---
+++ resumos-notas/01-fundamentos/poo.cs
@@ -0,0 +1,25 @@
+/*
+* Notas e códigos de estudo de C#
+* By João Paulo
+*/
+
+
+//=============== Object  ===============\\
+/*
+    Toda classe em C# é uma subclasse object
+    Object possui os seguintes metodos:
+    - GetType - Retorna o tipo do objeto.
+    - Equals - Compara se o objeto é igual ao outro.
+    - GetHash - Retorna uma código Hash do Objeto.
+    - ToString - Converte o Objeto para string.
+
+
+    ------------ ToString ------------
+    Usar o ToString em uma classe é necessário lançar mão de sobreposição, para isso usamos o override.
+    Ex:
+        public override string ToString(){
+            return "retorno da classe aqui";
+        }
+
+
+*/
