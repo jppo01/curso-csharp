1e83e043f8989a50a89dc2d703a99646b38d851b
João Paulo <jppo@live.com>
2022-07-17 16:15:46 -0300

    Adicionado exercícios 01 de matriz

---
+++ CURSO_UDEMY_C#Completo/exercicios-resolvidos/arrays-listas-matrizes/diagonal-matriz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+int N = 0, negativeNumbers = 0;
+System.Console.WriteLine("Valor de N: ");
+N = int.Parse(Console.ReadLine());
+
+double[,] mat = new double[N,N];
+
+//Ler valores da matriz
+for (int i = 0; i<N; i++){
+    for(int j=0; j<N; j++){
+        System.Console.WriteLine("Elemento " + (i+1) + "x" + (j+1) + ":");
+        mat[i,j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    }
+}
+
+//Printar diagonal da matriz e quantos números negativos
+System.Console.WriteLine("Diagonal Principal: ");
+for (int k = 0; k<N; k++){
+    for(int y=0; y<N; y++){
+        if (mat[k,y] < 0){
+            negativeNumbers += 1; 
+        }
+    }
+        System.Console.Write(mat[k,k] + " ");
+        System.Console.WriteLine();
+}
+System.Console.WriteLine("Quantidade de Números Negativos na Matriz = " + negativeNumbers);
